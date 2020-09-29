f16f363debf3de78b4bdb5e443f4858fdd70cb06
Hrittika103 <hrittika.paul.uemk.cse2016@gmail.com>
2020-09-29 13:05:45 +0530

    fc

---
+++ src/Date.java
@@ -0,0 +1,29 @@
+
+public class Date {
+
+	int dd , mm , yy ;
+
+public void setDtae(int d , int m , int y)
+{
+    dd=d;
+    mm=m;
+    yy=y;
+} 
+public int getDd()
+{
+	return dd;
+	
+}
+public int getMm()
+{
+	return mm;
+}
+public int getYy()
+{
+	return yy;
+}
+public void displayDate()
+{
+	System.out.println("Date is :"+dd+","+mm+","+yy);
+}
+}
---
+++ src/User.java
@@ -0,0 +1,12 @@
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d1 = new Date();
+		d1.setDtae(7,9,20);
+		d1.displayDate();
+
+	}
+
+}
